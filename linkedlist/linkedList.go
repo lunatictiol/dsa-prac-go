d946cdd5ca3b142dd84bbfcba34a67fd4571e551
lunatictiol <bhatsabiq9@gmail.com>
2024-12-10 20:15:13 +0530

    linkedlist

---
+++ linkedlist/linkedList.go
@@ -0,0 +1,91 @@
+package linkedlist
+
+import (
+	"fmt"
+)
+
+type node struct {
+	value interface{}
+	next  *node
+}
+type LinkedList struct {
+	head   *node
+	Length int
+	tail   *node
+}
+
+func (list *LinkedList) Append(value any) {
+	newNode := &node{value: value, next: nil}
+	if list.head == nil {
+		list.head = newNode
+		list.tail = list.head
+		return
+	}
+	list.tail.next = newNode
+	list.tail = newNode
+	list.Length++
+}
+
+func (list *LinkedList) Prepend(value any) {
+
+	newNode := &node{value: value, next: list.head}
+	list.head = newNode
+	list.Length++
+
+}
+
+func (list *LinkedList) Insert(index int, value any) {
+	if index <= 0 {
+		list.Prepend(value)
+		return
+	}
+	if index >= list.Length {
+		list.Append(value)
+		return
+	}
+	newNode := &node{
+		value: value,
+		next:  nil,
+	}
+	currentNode := list.head
+	for current := 0; current < index-1; current++ {
+		currentNode = currentNode.next
+	}
+	newNode.next = currentNode.next
+	currentNode.next = newNode
+
+}
+
+func (list *LinkedList) Delete(index int) {
+	if index == 0 {
+		list.head = list.head.next
+		list.Length--
+		return
+	}
+	currentNode := list.head
+	for current := 0; current < index-1; current++ {
+		currentNode = currentNode.next
+	}
+
+	currentNode.next = currentNode.next.next
+	list.Length--
+
+}
+
+func (list *LinkedList) Print() {
+	var current *node = list.head
+	for current != nil {
+		fmt.Println(current.value)
+		current = current.next
+	}
+	fmt.Println()
+}
+
+func (list *LinkedList) Get(index int) any {
+	currentNode := list.head
+	for current := 0; current < index-1; current++ {
+		currentNode = currentNode.next
+	}
+	return currentNode.next.value
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/lunatictiol/dsa-prac-go/questions"
+	"github.com/lunatictiol/dsa-prac-go/linkedlist"
 )
 
 func main() {
@@ -43,6 +43,21 @@ func main() {
 	// questions.Rotate(nums, 7)
 	// fmt.Println(nums)
 	//questions.PlusOne([]int{7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6})
-	questions.SingleNumberWithXor([]int{4, 1, 2, 1, 2})
+	//questions.SingleNumberWithXor([]int{4, 1, 2, 1, 2})
+	list := &linkedlist.LinkedList{}
+
+	list.Append(2)
+	list.Append(3)
+	list.Prepend(1)
+	list.Append(4)
+	list.Prepend(0)
+	list.Insert(1, 7)
+	list.Insert(3, 8)
+	list.Insert(7, 10)
+	//	n := list.Get(3)
+	//fmt.Println(n)
+	list.Print()
+	list.Delete(0)
+	list.Print()
 
 }
